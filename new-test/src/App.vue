84f61d5ba6522220ef16f0647edc57dc719eac50
Matvey <79156175+Morjick@users.noreply.github.com>
2021-02-18 15:01:17 +0300

    reake

---
+++ new-test/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+      
+      
+       <singin />
+
+
+    
+
+  </div>
+</template>
+
+<script>
+import singin from '@/components/singin'
+//import personal from '@/components/personal'
+
+
+export default {
+  name: 'App',
+  components: {
+    singin
+  },
+}
+</script>
+
+<style>
+
+</style>
